d90cf81cee104c0de96d446b1099d20bbb64da8d
Muhammad Ridho <helloshiftroll@gmail.com>
2025-11-24 05:37:14 +0700

    commit

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -10,11 +10,21 @@ interface PageLayoutProps {
 export function PageLayout({ sidebar, children, rightPanel }: PageLayoutProps) {
     return (
         <div className="flex min-h-[calc(100vh-3.5rem)]">
-            <Sidebar>{sidebar}</Sidebar>
+            {sidebar ? (
+                <aside className="w-64 bg-background border-r border-border h-screen flex flex-col sticky top-0">
+                    {sidebar}
+                </aside>
+            ) : (
+                <Sidebar />
+            )}
             <main className="flex-1 min-w-0 bg-white dark:bg-gray-950">
                 {children}
             </main>
-            <RightPanel>{rightPanel}</RightPanel>
+            {rightPanel && (
+                <aside className="w-80 bg-background border-l border-border h-screen sticky top-0 overflow-y-auto">
+                    {rightPanel}
+                </aside>
+            )}
         </div>
     );
 }
