208057d4fd9c6fd914c9070ab18e0dad3dca9a93
Muhammad Ridho <helloshiftroll@gmail.com>
2025-11-24 05:31:06 +0700

    commit

--- components/layout/PageLayout.tsx
+++ components/layout/PageLayout.tsx
@@ -1,4 +1,5 @@
-import { TopBar } from "./TopBar";
+import { Sidebar } from "./Sidebar";
+import { RightPanel } from "./RightPanel";
 
 interface PageLayoutProps {
     sidebar?: React.ReactNode;
@@ -8,23 +9,12 @@ interface PageLayoutProps {
 
 export function PageLayout({ sidebar, children, rightPanel }: PageLayoutProps) {
     return (
-        <div className="min-h-screen bg-background flex flex-col">
-            <TopBar />
-            <div className="flex min-h-[calc(100vh-4rem)]">
-                {sidebar && (
-                    <aside className="w-64 border-r border-border bg-background overflow-y-auto">
-                        {sidebar}
-                    </aside>
-                )}
-                <main className="flex-1 min-w-0 bg-white dark:bg-gray-950">
-                    {children}
-                </main>
-                {rightPanel && (
-                    <aside className="w-80 border-l border-border bg-background overflow-y-auto">
-                        {rightPanel}
-                    </aside>
-                )}
-            </div>
+        <div className="flex min-h-[calc(100vh-3.5rem)]">
+            <Sidebar>{sidebar}</Sidebar>
+            <main className="flex-1 min-w-0 bg-white dark:bg-gray-950">
+                {children}
+            </main>
+            <RightPanel>{rightPanel}</RightPanel>
         </div>
     );
 }
